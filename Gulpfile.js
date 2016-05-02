c78bea81fb5f35737afacf1d7287972bdc027522
Fabio <fabio.ottaviani@gmail.com>
2016-05-02 20:50:38 +0200

    Frontend Init. Includes complete tooling using Gulp (SCSS, Autoprefixer, JS & livereload server) and some useful CDN.

---
+++ Gulpfile.js
@@ -0,0 +1,93 @@
+'use strict';
+
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    sass = require('gulp-sass'),
+    rename = require("gulp-rename"),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssmin = require('gulp-cssmin'),
+    uglify = require('gulp-uglify'),
+    webserver = require('gulp-webserver');
+
+
+/***********
+*** SASS ***
+************/
+gulp.task('sass', function () {
+    console.log('COMPILING SASS');
+    return gulp.src('./css/**/*.scss')
+        .pipe(plumber(function (error) {
+            console.log('sass error: compile plumber', error);
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(rename({ dirname: '' }))
+        .pipe(gulp.dest('./css'))
+        // minify
+        .pipe(cssmin())
+		.pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./css'));
+});
+
+
+/*****************
+*** SASS WATCH ***
+******************/
+gulp.task('sass:watch', function () {
+    var watcher = gulp.watch('./css/**/*.scss', ['sass']);
+    watcher.on('change', function (e) {
+        console.log('watcher.on.change type: ' + e.type + ' path: ' + e.path);
+    });
+    return watcher;
+});
+
+
+/*********
+*** JS ***
+**********/
+gulp.task('js', function () {
+    console.log('MINIFYING JS');
+    return gulp.src('./js/main.js')
+        // minify
+        .pipe(uglify())
+		.pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./js'));
+});
+
+
+/***************
+*** JS WATCH ***
+****************/
+gulp.task('js:watch', function () {
+    var watcher = gulp.watch('./js/main.js', ['js']);
+    watcher.on('change', function (e) {
+        console.log('watcher.on.change type: ' + e.type + ' path: ' + e.path);
+    });
+    return watcher;
+});
+
+
+/****************
+*** WEBSERVER ***
+*****************/
+gulp.task('webserver', function () {
+    return gulp.src('./')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: false,
+            port: 8080,
+            open: true
+        }));
+});
+
+
+/************
+*** START ***
+*************/
+gulp.task('start', ['webserver', 'sass', 'sass:watch', 'js', 'js:watch']);
---
+++ README.md
@@ -0,0 +1,30 @@
+<<<<<<< HEAD
+# README
+
+## Features
+
+- Gulp setup with the following setup:
+    - SCSS compiling using libsass (including autoprefixer)
+    - Auto-refreshing dev server using gulp-livereload
+    - Javascript minification
+- HTML5 templated index.htm
+- Latest stable releases with CDNJS: Twitter Bootstrap 3, jQuery, Modernizr, Slick, Animate.css, Wow.js
+
+## Installation
+
+0. Check you have the required dependencies: Node ≥ v0.10 & npm ≥ 2.1.5 `node -v && npm -v`
+1. Clone the project `git clone https://github.com/supahfunk/frontend-init.git`
+2. Install NodeJS dependencies `npm install -d`
+
+## Usage
+
+1. Compile files, start watching files, and start dev server `npm start`
+2. Open the dev server in your brower `localhost:8888` also available on the network using `ComputerName.local:8888`
+
+## License
+
+supahfunk/frontend-init is released under the MIT license.
+=======
+# frontend-init
+Frontend Init. Includes complete tooling using Gulp (SCSS, Autoprefixer, JS &amp; livereload server) and some useful CDN.
+>>>>>>> 7bf1a0c3daa5b12cf9764b947c89c39dfe51bf6b
